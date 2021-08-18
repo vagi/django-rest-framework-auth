e6e8ca38c260a127e858f6378e043e57e680ba36
Vagif Molla Air <vagif69@gmail.com>
2021-08-19 00:09:55 +0300

    Add path api-token-auth

--- myapi/urls.py
+++ myapi/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token # <---
 from myapi.core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello', views.HelloView.as_view(), name='hello'),
+    path('hello/', views.HelloView.as_view(), name='hello'),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  # <----
 ]
